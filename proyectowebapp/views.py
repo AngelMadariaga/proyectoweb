50b958c1c14611595b320e8263fc38789adf98a5
Angel Madariaga <angel.madariaga@megaterios.co>
2022-04-07 12:14:25 -0500

    First commit

---
+++ proyectowebapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render , httpresponse
+
+# Create your views here.
+
+def home(request):
+    
+    return httpresponse ("inicio")
+    
+ def home(request):
+        
+    return httpresponse ("inicio")
+
+
+def home(request):
+    
+    return httpresponse ("inicio")    
