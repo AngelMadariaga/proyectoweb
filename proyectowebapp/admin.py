b68d02266e3ff21e027fbc9209b2bacb4e7aaeb6
Angel Madariaga <angel.madariaga@megaterios.co>
2022-04-18 18:01:07 -0500

    fitst commit

--- proyectowebapp/admin.py
+++ proyectowebapp/admin.py
@@ -1,26 +1,3 @@
 from django.contrib import admin
-from .models import ArticleLikes, CommentLikes, SubComment, User,Article,Comment
-
+from django.conf import views.py
 # Register your models here.
-
-class ArticleAdmin(admin.ModelAdmin):
-    list_display=("id","author","title","date","likes","dislikes","comments")
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display=("id","author","article","comment","likes","dislikes")
-    
-class SubCommentAdmin(admin.ModelAdmin):
-    list_display=("id","commentFather","author","article","subcomment")
-    
-class ArticleLikeAdmin(admin.ModelAdmin):
-    list_display=("id","author","article","value")
-    
-class CommentLikeAdmin(admin.ModelAdmin):
-    list_display=("id","author","comment","value")
-    
-admin.site.register(User)
-admin.site.register(Article,ArticleAdmin)
-admin.site.register(Comment,CommentAdmin)
-admin.site.register(SubComment,SubCommentAdmin)
-admin.site.register(ArticleLikes,ArticleLikeAdmin)
-admin.site.register(CommentLikes,CommentLikeAdmin)
