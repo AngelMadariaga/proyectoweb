d99bf707406918fa8e91d4109d4c00c61d00d087
Angel Madariaga <angel.madariaga@megaterios.co>
2022-04-26 19:37:13 -0500

    articulos

---
+++ proyectowebapp/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Blog'
+    
+ class Blogconfig(AppConfig) :
+     defailt_auto_field ='django.db.modelos.BigAutosField'
--- proyectowebapp/urls.py
+++ proyectowebapp/urls.py
@@ -16,16 +16,4 @@ urlpatterns = [
     path('conctatos' ,views.conctatos , name="conctatos" ),
     path('blog' ,views.blog , name="blog" ),
     
-]
-
-from  django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationFrom
-from django.views-from django.views.generic import CreateView
-from django.core.urlresolvers import reverse_lazy
-
-
-class registriousuario(creataview):
-    .model= user
-    template_name="usuario/registrar.html"
-    from_class = UserCreationForm
-    success_url =reverse_lazy('mascotas:mascota_lista')
+    
