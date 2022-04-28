6bf8d1ceb34769a7475165f84b3a609f1c093a0a
Angel Madariaga <angel.madariaga@megaterios.co>
2022-04-27 19:42:31 -0500

    Formulario

--- proyectowebapp/models.py
+++ proyectowebapp/models.py
@@ -1,33 +1,102 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.forms import IntegerField
+
 
 # Create your models here.
-from django.db import models
-from django.forms import ModelForm
+class User(AbstractUser):
+    image=models.ImageField(upload_to='foto_autores',null=True)
+    
+    email = models.EmailField(('email address'), unique=True)
 
-TITLE_CHOICES = [
-    ('MR', 'Mr.'),
-    ('MRS', 'Mrs.'),
-    ('MS', 'Ms.'),
-]
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    class Meta:
+        verbose_name='Usuario'
+        verbose_name_plural='Usuarios'
+    
+    def __str__(self):
+        return self.email
+      
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-    title = models.CharField(max_length=3, choices=TITLE_CHOICES)
-    birth_date = models.DateField(blank=True, null=True)
+class Article(models.Model):
+    author=models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Autor')
+    title=models.CharField(max_length=100,verbose_name='Titulo')
+    photo=models.ImageField(upload_to='foto_articulos',verbose_name='imagen')
+    introduction=models.CharField(max_length=450, verbose_name='Introduccion')
+    content=models.CharField(max_length=2500, verbose_name='Contenido')
+    date=models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creacion')
+    likes=models.IntegerField(verbose_name='Me gusta', default=0)
+    dislikes=models.IntegerField(verbose_name='No me gusta',default=0)
+    comments=models.IntegerField(verbose_name='Comentarios',default=0)
+    
+    class Meta:
+        verbose_name='Articulo'
+        verbose_name_plural='Articulos'
 
     def __str__(self):
-        return self.name
+        return self.title
 
-class Book(models.Model):
-    name = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Author)
 
-class AuthorForm(ModelForm):
+class Comment(models.Model):
+    author=models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Autor')
+    article=models.ForeignKey(Article, on_delete=models.CASCADE,verbose_name='Articulo')
+    date=models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creacion')
+    comment=models.CharField(max_length=300, verbose_name='Comentario')
+    likes=models.IntegerField(verbose_name='Me gusta', default=0)
+    dislikes=models.IntegerField(verbose_name='No me gusta',default=0)
+    
     class Meta:
-        model = Author
-        fields = ['name', 'title', 'birth_date']
-
-class BookForm(ModelForm):
+        verbose_name='Comentario'
+        verbose_name_plural='Comentarios'
+    
+    def __str__(self):
+        return self.comment
+       
+    
+class SubComment(models.Model):
+    commentFather=models.ForeignKey(Comment, on_delete=models.CASCADE, verbose_name='Comentario Padre')
+    author=models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Autor')
+    article=models.ForeignKey(Article, on_delete=models.CASCADE,verbose_name='Articulo')
+    subcomment=models.CharField(max_length=300, verbose_name='Sub Comentario')
+    date=models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creacion')
+    likes=models.IntegerField(verbose_name='Me gusta', default=0)
+    dislikes=models.IntegerField(verbose_name='No me gusta',default=0)
+    
     class Meta:
-        model = Book
-        fields = ['name', 'authors']
+        verbose_name='Sub Comentario'
+        verbose_name_plural='Sub Comentarios' 
+        verbose_name_plural=
+
+    
+    def __str__(self):
+        return self.subcomment
+    
+    
+class ArticleLikes(models.Model):
+    article=models.ForeignKey(Article,on_delete=models.CASCADE)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    value=models.IntegerField(default=0)
+    
+    class Meta:
+        verbose_name='Articulos Like'
+        verbose_name_plural='Articulos Likes'
+
+
+class CommentLikes(models.Model):
+    comment=models.ForeignKey(Comment,on_delete=models.CASCADE)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    value=models.IntegerField(default=0)
+    
+    class Meta:
+        verbose_name='Comentario Like'
+        verbose_name_plural='Comentarios Likes'
+    
+
+class SubcommentLikes(models.Model):
+    subcomment=models.ForeignKey(SubComment,on_delete=models.CASCADE)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    value=models.IntegerField(default=0)
+    
--- proyectowebapp/urls.py
+++ proyectowebapp/urls.py
@@ -1,19 +1,24 @@
-from django.urls import path
-from.views import home, blog, formulario
-
-
 from django.contrib import admin
-
+from django.urls import path
+from .views import *
+from django.contrib.auth.views import logout_then_login
 
 urlpatterns = [
-
- url(r'admin', admin.site.urls ),
- url(r'usuario' , include('apps.usuario.urls', namespace="usuario")),
- 
-     
-    path('' ,views.home , name="home" ),
-    path('registro' ,views.registro , name="registro" ),
-    path('conctatos' ,views.conctatos , name="conctatos" ),
-    path('blog' ,views.blog , name="blog" ),
+    path('',Articulos, name='articulos'),
+    path('accounts/login/',Inicio De Sesion.as_view(), name='login'),
+    path('logout/', logout_then_login, name='logout'),
+    path('registrarme/',Registro.as_view(), name='registro'),
+    path('mis-articulos/<int:pk>',MisArticulos, name='mis_articulos'),
+    path('nuevo-articulo/',NuevoArticulo.as_view(), name='nuevo_articulo'),
+    path('editar-articulo/<int:pk>',EditarArticulo, name='editar_articulo'),
+    path('eliminar-articulo/',EliminarArticulo, name='eliminar_articulo'),
+    path('<int:pk>/<str:title>/',DetalleArticulo, name='detalle_articulo'),
+    path('comentario/',Comentar, name='nuevo_comentario'),
+    path('sub-comentario/',SubComentar, name='nuevo_subcomentario'),
+    path('article-nuevo-like/<int:value>/<int:pk>/',ArticleLike, name='nuevo_like'),
+    path('article-nuevo-dislike/<int:value>/<int:pk>/',ArticleDislike, name='nuevo_dislike'),
+    path('comment-nuevo-like/<int:value>/<int:pk>/<str:title>/<int:pka>/',CommentLike, name='nuevo_likec'),
+    path('comment-nuevo-dislike/<int:value>/<int:pk>/<str:title>/<int:pka>/',CommentDislike, name='nuevo_dislikec'),
+]  
     
     
